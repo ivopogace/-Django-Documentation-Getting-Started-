7a4db9d49e12bfad54a174653689b40bf9808363
Ivo <ksh>
2020-11-27 23:05:01 +0100

    django api documentation part 7 finished

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-
 from django.contrib import admin
 
 # Register your models here.
-from polls.models import Question, Car
+from polls.models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Car)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    """
+    The first element of each tuple in fieldsets is the title of the fieldset. Here’s what our form looks like now:
+    """
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [(None, {'fields': ['question_text']}), ('Date information', {'fields': ['pub_date']}), ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    list_per_page = 10
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
     def __str__(self):
         return self.question_text
@@ -26,10 +29,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-
-class Car(models.Model):
-    color = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.color
