4b020ffeb42512ae62008e65ff369455a0401264
Ivo <ksh>
2020-11-27 12:21:31 +0100

    django api doc part 4 1/2

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,9 @@
+from django.shortcuts import render
+
 from django.contrib import admin
 
 # Register your models here.
+from polls.models import Question, Car
+
+admin.site.register(Question)
+admin.site.register(Car)
